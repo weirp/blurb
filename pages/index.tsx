1b8f1c1d6dbc243153e5a2452ad148eb9d3d1bd0
Philip Weir <philip.john.weir@gmail.com>
2023-06-01 16:46:21 +0800

    end of module 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,48 @@
-import { Button, Stack, TextField, Typography } from "@mui/material";
+import { Button, Stack, TextField, Typography, Card, CardContent } from "@mui/material";
+import { useRef, useCallback, useState } from "react";
+
 
 export default function Home() {
-  function generateBlurb(): void {
-      throw new Error("Function not implemented.");
-  }
+  const blurbRef = useRef("");
+  const [generatingPosts, setGeneratingPosts] = useState("")
+  const prompt = `Generate 3 tweets and clearly labeled "1." , "2." and "3.". 
+     Follow the following criteria:
+        1. Each tweet should be based on this context: ${blurbRef.current}
+        2. Each tweet will have short sentences that are found in Twitter posts. 
+        3. Each tweet will be strictly less than 280 tokens including spaces, punctuation, emojis and hashtags`;
+
+
+  const generateBlurb = useCallback(async () => {
+    let done = false;
+    const response = await fetch("/api/generateBlurb", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: prompt, //blurbRef.current,
+      }),
+    });
+    if (!response.ok) {
+          throw new Error(response.statusText);
+    }
+    const data = response.body;
+    if (!data) {
+            return;
+          }
+          const reader = data.getReader();
+          const decoder = new TextDecoder();
+      
+      
+      
+          while (!done) {
+            const { value, done: doneReading } = await reader.read();
+            done = doneReading;
+            const chunkValue = decoder.decode(value);
+            setGeneratingPosts((prev) => prev + chunkValue);
+          }
+      }
+  , [blurbRef.current]);
 
   return (
     <Stack
@@ -27,11 +66,32 @@ export default function Home() {
         multiline
         fullWidth
         minRows={4}
+        onChange={(e) => {
+                blurbRef.current = e.target.value;
+              }}
         sx={{ "& textarea": { boxShadow: "none !important" } }}
         placeholder="Key words on what you would like your blurb to be about"
       ></TextField>
 
     <Button onClick={generateBlurb}>Generate Blurb</Button>
+    
+    {generatingPosts && (
+      <Card>
+        <CardContent>{generatingPosts}</CardContent>
+      </Card>
+    )}
+    
+    {generatingPosts &&
+              generatingPosts
+                .substring(generatingPosts.indexOf("1.") + 3)
+                .split(/2\.|3\./)
+                .map((generatingPost, index) => {
+                  return (
+                    <Card key={index}>
+                      <CardContent>{generatingPost}</CardContent>
+                    </Card>
+                  );
+                })}
     
    </Stack>
   );
